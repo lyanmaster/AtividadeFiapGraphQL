ad75a7c4655841b98b94e941814e02ec8de1b8c0
Lyan Masterson Pereira Miguel <lyan_master@hotmail.com>
2020-12-22 11:48:34 -0300

    Adiciona mutation para excluir e alterar produtos.

--- functions/resolvers/Produto.js
+++ functions/resolvers/Produto.js
@@ -34,5 +34,53 @@ module.exports = {
         .then((snap) => snap.val())
         .then((val) => Object.keys(val).map((key) => val[key]));
     },
+    excluirProduto(_, { id }) {
+      return admin
+        .database()
+        .ref("produtos")
+        .orderByChild("id")
+        .equalTo(id)
+        .once("value")
+        .then((snap) => snap.val())
+        .then((val) => {
+          if (val) {
+            const firstKey = Object.keys(val)[0];
+            admin.database().ref("produtos").child(firstKey).remove();
+            return val[firstKey];
+          }
+        });
+    },
+    alterarProduto(
+      _,
+      { id, nomeproduto, descricao, fornecedor, preco, datacadastro }
+    ) {
+      return admin
+        .database()
+        .ref("produtos")
+        .orderByChild("id")
+        .equalTo(id)
+        .once("value")
+        .then((snap) => snap.val())
+        .then((val) => {
+          if (val) {
+            const firstKey = Object.keys(val)[0];
+            let produtoAlterado = val[firstKey];
+            produtoAlterado.nomeproduto = nomeproduto;
+            produtoAlterado.descricao = descricao;
+            produtoAlterado.fornecedor = fornecedor;
+            produtoAlterado.preco = preco;
+            produtoAlterado.datacadastro = datacadastro;
+
+            console.log("produtoAlterado", produtoAlterado);
+
+            admin
+              .database()
+              .ref("produtos")
+              .child(firstKey)
+              .set(produtoAlterado);
+            return produtoAlterado;
+          }
+        });
+    },
   },
 };
